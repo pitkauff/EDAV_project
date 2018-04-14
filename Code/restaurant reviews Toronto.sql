27b6e9a19a8d5742b5adc8ddf63fb9a8d41786e4
Carlo <carloprovinciali@gmail.com>
2018-04-14 16:00:23 -0400

    added asian fusion and caraibbean

--- Code/restaurant reviews Toronto.sql
+++ Code/restaurant reviews Toronto.sql
@@ -15,7 +15,34 @@ INNER JOIN (SELECT b1.*
 			FROM business b1
 			INNER JOIN category c1
 				ON c1.business_id = b1.id
-			WHERE c1.category = 'Restaurants'
+			WHERE c1.category IN (    	'American (New)',
+										'American (Traditional)',
+                                        'Asian Fusion',
+										'Cajun/Creole',
+										'Canadian (New)',
+										'Cantonese',
+                                        'Caraibbean',
+										'Chinese',
+										'Cuban',
+										'Ethiopian',
+										'French',
+										'Greek',
+										'Halal',
+										'Indian',
+										'Indonesian',
+										'Italian',
+										'Japanese',
+										'Korean',
+										'Malaysian',
+										'Mexican',
+										'Middle Eastern',
+										'Pakistani',
+										'Peruvian',
+										'Singaporean',
+										'Syrian',
+										'Thai',
+										'Turkish',
+										'Vietnamese')
 			AND   b1.city = 'Toronto'
             AND   b1.state = 'ON') b
 	ON r.business_id = b.id
