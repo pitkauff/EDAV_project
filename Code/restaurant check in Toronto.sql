1b48a2accdfc29f1896fd7da214896d04e983ad7
Carlo <carloprovinciali@gmail.com>
2018-04-15 20:37:09 -0400

    added checkins

---
+++ Code/restaurant check in Toronto.sql
@@ -0,0 +1,37 @@
+SELECT c.id, c.business_id, c.`date`, c.`count`
+from checkin c
+INNER JOIN (SELECT b1.id
+			FROM business b1
+			INNER JOIN category c1
+				ON c1.business_id = b1.id
+			WHERE c1.category IN (    	'American (New)',
+										'American (Traditional)',
+                                        'Asian Fusion',
+										'Cajun/Creole',
+										'Canadian (New)',
+										'Cantonese',
+                                        'Caraibbean',
+										'Chinese',
+										'Cuban',
+										'Ethiopian',
+										'French',
+										'Greek',
+										'Halal',
+										'Indian',
+										'Indonesian',
+										'Italian',
+										'Japanese',
+										'Korean',
+										'Malaysian',
+										'Mexican',
+										'Middle Eastern',
+										'Pakistani',
+										'Peruvian',
+										'Singaporean',
+										'Syrian',
+										'Thai',
+										'Turkish',
+										'Vietnamese')
+			AND   b1.city = 'Toronto'
+            AND   b1.state = 'ON') b
+		ON c.business_id = b.id
