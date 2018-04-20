112388e2203aecfd07d5f6c3614594be518e5253
pitkauff <pitkauff@umich.edu>
2018-04-19 23:50:29 -0400

    Add files via upload

---
+++ Code/server.R
@@ -0,0 +1,18 @@
+# server.R
+library(ggmap)
+library(ggplot2)
+library(shiny)
+library(dplyr)
+
+#setwd("~/Desktop/Columbia Files/Spring 2018/Exploratory Data Analysis/Project/EDAV_project-master/Data")
+#stuff <- read.csv("Toronto_revs_area.csv", header = TRUE, sep = ",")
+
+server <- function(input, output, session) {
+  output$userpanel <- renderUI({
+    if (!is.null(session$user)) {
+      sidebarUserPanel(
+        span("Logged in as ", session$user),
+        subtitle = a(icon("sign-out"), "Logout", href="__logout__"))
+    }
+  })
+}
