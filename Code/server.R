386737a69583394eadff4d7372cf8e699a15cd55
pitkauff <pitkauff@umich.edu>
2018-04-19 23:51:23 -0400

    Add files via upload

--- Code/server.R
+++ Code/server.R
@@ -1,18 +1,68 @@
 # server.R
 library(ggmap)
 library(ggplot2)
+library(readr)
 library(shiny)
 library(dplyr)
+library(plotly)
+library(rlang)
 
-#setwd("~/Desktop/Columbia Files/Spring 2018/Exploratory Data Analysis/Project/EDAV_project-master/Data")
-#stuff <- read.csv("Toronto_revs_area.csv", header = TRUE, sep = ",")
+Sys.setenv('MAPBOX_TOKEN' = 'pk.eyJ1IjoicGl0a2F1ZmYiLCJhIjoiY2pnNG10OWkxMG5jZjJ3cWQ2N2JlbnVhayJ9.ZS4VPpOc5iBycmkEkAVf-w')
+setwd("~/Desktop/Columbia Files/Spring 2018/Exploratory Data Analysis/Project/Data")
+data <- read_csv("Toronto_revs_area.csv")
+colnames(data)[10] <- "sentiment"
+colnames(data)[19] <- "rating"
+data <- data %>% dplyr::group_by(id) %>% mutate(sent = mean(sentiment)) %>% mutate(rat = mean(rating))
+stuff <- data[!duplicated(data$id),]
+setwd("~/Desktop/Columbia Files/Spring 2018/Exploratory Data Analysis/Project/Data")
+geo <- read.delim("GeoHash.txt", sep = ",")
+geo <- prepend(levels(geo$Area), "All")
+setwd("~/Desktop/Columbia Files/Spring 2018/Exploratory Data Analysis/Project/Data")
+categories <- read.delim("Cat_reduced.txt", sep = ",")
+categories <- prepend(levels(categories$Category), "All")
 
-server <- function(input, output, session) {
-  output$userpanel <- renderUI({
-    if (!is.null(session$user)) {
-      sidebarUserPanel(
-        span("Logged in as ", session$user),
-        subtitle = a(icon("sign-out"), "Logout", href="__logout__"))
+function(input, output) {
+  
+  output$TorontoMap <- renderPlotly({
+    
+    if (input$area == "All" & input$cuisine == "All") {
+      subset <- reactive({stuff
+      })
+    } else if (input$area == "All") {
+      subset <- reactive({dplyr::filter(stuff, category == input$cuisine)})
+    } else if (input$cuisine == "All") {
+      subset <- reactive({dplyr::filter(stuff, geohash == input$area)
+      })
+    } else {
+      subset <- reactive({dplyr::filter(stuff, geohash == input$area &
+                                        category == input$cuisine)
+      })
     }
+    
+    tmp <- subset()
+    lon_range <- extendrange(tmp$longitude)
+    lat_range <- extendrange(tmp$latitude)
+    zo <- calc_zoom(lon_range, lat_range)
+    p <- plot_mapbox(subset(), x = ~longitude, y = ~latitude, mode = "markers", type = "scatter") %>%
+      add_markers(
+        color = ~sent, size = I(8), alpha = 0.7, hoveron = "text",
+        text = ~paste(paste("Name: ",name),
+                      paste("Sentiment: ", round(as.numeric(sent),2)), 
+                      paste("Rating: ", rating), sep = "<br />")) %>%
+      colorbar(title = "Average Review Sentiment") %>%
+      layout(mapbox = list(zoom = zo - 2,
+                           center = list(lat = ~median(latitude),
+                                         lon = ~median(longitude))
+      ))
+    
+
+  })# height = 600, width = 800)
+ 
+  output$barchart <- renderPlot({
+    
+    }) #height = 300, width = 825)
+  
+  output$surpriseNote <- renderText({
+  
   })
 }
--- Code/ui.R
+++ Code/ui.R
@@ -1,76 +1,59 @@
-#ui.R
+# ui.R
 library(ggmap)
+library(readr)
 library(ggplot2)
 library(shiny)
 library(dplyr)
-library(shinydashboard)
-  
-#setwd("~/Desktop/Columbia Files/Spring 2018/Exploratory Data Analysis/Project/EDAV_project-master/Data")
-#stuff <- read.csv("Toronto_revs_area.csv", header = TRUE, sep = ",")
+library(plotly)
+library(rlang)
 
-ui <- dashboardPage(skin = "yellow",
-  dashboardHeader(
-    title = tags$img(src = 'ds.png', height = 30, width = 180)
-  ),
-  dashboardSidebar(
-    tags$head(tags$style(HTML('.shiny-server-account { display: none; }'))),
-    uiOutput("userpanel"),
-    sidebarMenu(
-      menuItem("Introduction", tabName = "intro", icon = icon("dashboard")),
-      menuItem("Data Description", tabName = "data", icon = icon("table"),
-        menuSubItem("Methodolody", tabName = "meth1", icon = icon("chevron-circle-right")),
-        menuSubItem("Data", tabName = "data1", icon = icon("chevron-circle-right"))),
-      menuItem("Data Quality Analysis", tabName = "quality", icon = icon("map")),
-      menuItem("Main Analysis", tabName = "main", icon = icon("map-marker")),
-      menuItem("Exective Summary", tabName = "revs", icon = icon("thumbs-up")),
-      menuItem("Interactive", tabName = "interactive", icon = icon("mouse-pointer")),
-      menuItem("Conclusion", tabName = "conclusion", icon = icon("table"))
-    )
-  ),
-  dashboardBody(
-    tabItems(
-      # First tab content
-      tabItem(tabName = "intro",
-          box(
-          h2("Welcome to the Toronto Restaurant Explorer", style = "font-weight: bold"),
-          p(HTML(paste0('For this project, we have focused our attention on the Yelp dataset, found ',
-                        a(href = 'https://www.yelp.com/dataset', 'here. '), 
-                        "After careful consideration of multiple dataset, we concluded that
-                        certain ones, such as Citibike or the Million Songs dataset have been
-                        overly analyzed and dissected, thereby limiting the vlaue we could add. We then
-                        started searchin for more low-key datasets and eventually came across the Yelp dataset. 
-                        Given our own experiences, we felt Yelp reviews were very tangible, widely 
-                        used and appreciated, and so we decided to dive deep into the dataset 
-                        in the hope of discovering interesting insights and patterns. Given the size 
-                        size of the full datset (over 100o cities across the US and Canada), we 
-                        decided to focus on a specific city, namely Toronto. Some high-level 
-                        questions we attempted to answer were as follows:")), style = "font-size:16px", align = "justify"),
-          width = 9,
-          tags$ul(tags$li("Where are the best restaurants of a particular cuisine located?"),
-                  tags$li("Which areas are most diverse in terms of cuisine?"),
-                  tags$li("Does a business with a high number of check-ins have a higher average rating?"),
-                  tags$li("How are rating and review sentiment related?"),
-                  tags$li("How do elite users affect rating and customer flow?"),
-                  tags$li("How do check-ins vary over the course of the week?")), style = "font-size:16px"),
-          fluidRow(
-          #box(plotOutput("plot1", height = 250)),
-          #box(
-          #  title = "Controls",
-          #  sliderInput("slider", "Number of observations:", 1, 100, 50)
-          #)
-        )
-      ),
-      
-      # Second tab content
-      tabItem(tabName = "meth1",
-          box(
-          h2("Data Collection / Preprocessing Overview", style = "font-weight: bold"),
-          p("Our dataset was directly queried from the Yelp database using SQLite. This ensured that 
-            the data came with a pre-defined schema, saving us a lot of data cleaning time. A high-
-            level outline of the schema is depcited below: ", align = "justify"),
-          img(src = 'yelp_dataset_schema.png', width = 600, height = 300, align = "center"),
-          width = 9)
+Sys.setenv('MAPBOX_TOKEN' = 'pk.eyJ1IjoicGl0a2F1ZmYiLCJhIjoiY2pnNG10OWkxMG5jZjJ3cWQ2N2JlbnVhayJ9.ZS4VPpOc5iBycmkEkAVf-w')
+setwd("~/Desktop/Columbia Files/Spring 2018/Exploratory Data Analysis/Project/Data")
+data <- read_csv("Toronto_revs_area.csv")
+colnames(data)[10] <- "sentiment"
+colnames(data)[19] <- "rating"
+data <- data %>% group_by(id) %>% mutate(sent = mean(sentiment))
+stuff <- data[!duplicated(data$id),]
+setwd("~/Desktop/Columbia Files/Spring 2018/Exploratory Data Analysis/Project/Data")
+geo <- read.delim("GeoHash.txt", sep = ",")
+geo <- prepend(levels(geo$Area), "All")
+setwd("~/Desktop/Columbia Files/Spring 2018/Exploratory Data Analysis/Project/Data")
+categories <- read.delim("Cat_reduced.txt", sep = ",")
+categories <- prepend(levels(categories$Category), "All")
+
+shinyUI(fluidPage(
+  titlePanel("New York City Felony Visualizer"),
+  
+  sidebarLayout(
+    sidebarPanel(
+      h3("This application lets you explote Toronto's reatsurant scene"),
+      selectInput("area", label = h4("Select an Area"), 
+                  choices =  geo, selected = "Downtown"),
+      br(),
+      selectInput("cuisine", label = h4("Select a Cuisine"), 
+                  choices = categories, selected = "Chinese")
+      #br(),
+      #radioButtons("month", label = h4("Select a month (Oct., Nov., Dec. not available",icon("thumbs-o-down"),")"), 
+      #             choices = c("Jan" = "Jan", "Feb" = "Feb", "Mar" = "Mar", "Apr" = "Apr", "May" = "May",
+      #                         "Jun" = "Jun", "Jul" = "Jul", "Aug" = "Aug", "Sep" = "Sep"), selected = "Jan"),
+      #br(),
+      #h4("Surprise Message"),
+      #checkboxInput("checkbox", label = "Check this box for a surprise message", value = FALSE),
+      #img(src = "picture.jpg", height = 250, width = 410)
+    ),
+    # mainPanel(leafletOutput("map", width="100%", height="100%"))
+    mainPanel(
+      plotlyOutput("TorontoMap", width="100%", height="100%"),
+      br(),
+      plotOutput("barchart", width="100%", height="100%"),
+      br(),
+      textOutput("surpriseNote"),
+      tags$head(tags$style("#surpriseNote{color: green;
+                                 font-size: 25px;
+                                 font-style: bold;
+                                 }"
+      )
       )
     )
   )
-)
+))
